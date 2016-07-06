e0f63ca5c1ed94195aa9d58586b3be4883167981
Kit Peters <kit.peters@broadbean.com>
2016-07-06 16:24:23 -0500

    Multi character tokens
    Synbol combinations such as '[(' and '[?(' are treated as single tokens

--- lib/JSON/Path/Compiler.pm
+++ lib/JSON/Path/Compiler.pm
@@ -6,42 +6,43 @@ use Readonly;
 our $AUTHORITY = 'cpan:POPEFELIX';
 our $VERSION   = '1.00';
 
-Readonly my $TOKEN_DOLLAR_SIGN          => '$';
-Readonly my $TOKEN_COMMERCIAL_AT        => '@';
-Readonly my $TOKEN_FULL_STOP            => '.';
-Readonly my $TOKEN_LEFT_SQUARE_BRACKET  => ']';
-Readonly my $TOKEN_RIGHT_SQUARE_BRACKET => '[';
-Readonly my $TOKEN_ASTERISK             => '*';
-Readonly my $TOKEN_COLON                => ':';
-Readonly my $TOKEN_LEFT_PARENTHESIS     => '(';
-Readonly my $TOKEN_RIGHT_PARENTHESIS    => ')';
-Readonly my $TOKEN_COMMA                => ',';
+Readonly my $DOLLAR_SIGN          => '$';
+Readonly my $COMMERCIAL_AT        => '@';
+Readonly my $FULL_STOP            => '.';
+Readonly my $LEFT_SQUARE_BRACKET  => '[';
+Readonly my $RIGHT_SQUARE_BRACKET => ']';
+Readonly my $ASTERISK             => '*';
+Readonly my $COLON                => ':';
+Readonly my $LEFT_PARENTHESIS     => '(';
+Readonly my $RIGHT_PARENTHESIS    => ')';
+Readonly my $COMMA                => ',';
+Readonly my $QUESTION_MARK        => '?';
 
-# JSONPath  Function
-# $         the root object/element
-# @         the current object/element
-# . or []   child operator
-# ..        recursive descent. JSONPath borrows this syntax from E4X.
-# *         wildcard. All objects/elements regardless their names.
-# n/a       attribute access. JSON structures don't have attributes.
-# []        subscript operator. XPath uses it to iterate over element collections and for predicates. In Javascript and JSON it is the native array operator.
-# [,]       Union operator in XPath results in a combination of node sets. JSONPath allows alternate names or array indices as a set.
-# [sta      rt:end:step]    array slice operator borrowed from ES4.
-# ?()       applies a filter (script) expression.
-# ()        script expression, using the underlying script engine.
+# JSONPath              Function
+# $                     the root object/element
+# @                     the current object/element
+# . or []               child operator
+# ..                    recursive descent. JSONPath borrows this syntax from E4X.
+# *                     wildcard. All objects/elements regardless their names.
+# []                    subscript operator. XPath uses it to iterate over element collections and for predicates. In Javascript and JSON it is the native array operator.
+# [,]                   Union operator in XPath results in a combination of node sets. JSONPath allows alternate names or array indices as a set.
+# [start:end:step]      array slice operator borrowed from ES4.
+# ?()                   applies a filter (script) expression.
+# ()                    script expression, using the underlying script engine.
+#
 # With JSONPath square brackets operate on the object or array addressed by the previous path fragment. Indices always start by 0.
 
-my %VALID_TOKENS = (
-    $TOKEN_DOLLAR_SIGN          => 1,
-    $TOKEN_COMMERCIAL_AT        => 1,
-    $TOKEN_FULL_STOP            => 1,
-    $TOKEN_LEFT_SQUARE_BRACKET  => 1,
-    $TOKEN_RIGHT_SQUARE_BRACKET => 1,
-    $TOKEN_ASTERISK             => 1,
-    $TOKEN_COLON                => 1,
-    $TOKEN_LEFT_PARENTHESIS     => 1,
-    $TOKEN_RIGHT_PARENTHESIS    => 1,
-    $TOKEN_COMMA                => 1,
+my %RESERVED_SYMBOLS = (
+    $DOLLAR_SIGN          => 1,
+    $COMMERCIAL_AT        => 1,
+    $FULL_STOP            => 1,
+    $LEFT_SQUARE_BRACKET  => 1,
+    $RIGHT_SQUARE_BRACKET => 1,
+    $ASTERISK             => 1,
+    $COLON                => 1,
+    $LEFT_PARENTHESIS     => 1,
+    $RIGHT_PARENTHESIS    => 1,
+    $COMMA                => 1,
 );
 
 my @STACK;
@@ -73,29 +74,27 @@ sub tokenize {
         my $char = shift @chars;
         last unless defined $char;
 
-        if ( !$VALID_TOKENS{$char} ) {
+        if ( !$RESERVED_SYMBOLS{$char} ) {
             my $token = $char;
 
             # Read from the character stream until we have a valid token
-            while ( 1 ) {
+            while (1) {
                 $char = shift @chars;
                 last unless defined $char;
 
-                if ( $VALID_TOKENS{$char} ) {
+                if ( $RESERVED_SYMBOLS{$char} ) {
                     unshift @chars, $char;
                     last;
                 }
                 $token .= $char;
             }
+            $token =~ s/^['|"](.+)['|"]$/\1/;    # Remove quotes
             push @tokens, $token;
         }
         else {
-            if ( $char ne $TOKEN_FULL_STOP ) {
-                push @tokens, $char;
-            }
-            elsif ( $char eq $TOKEN_FULL_STOP ) {    # Handle the ".." token
+            if ( $char eq $FULL_STOP ) {         # distinguish between the '.' and '..' tokens
                 my $next_char = shift @chars;
-                if ( $next_char eq $TOKEN_FULL_STOP ) {
+                if ( $next_char eq $FULL_STOP ) {
                     push @tokens, $char . $next_char;
                 }
                 else {
@@ -103,28 +102,83 @@ sub tokenize {
                     push @tokens, $char;
                 }
             }
+            elsif ( $char eq $LEFT_SQUARE_BRACKET ) {
+                my $token     = $char;
+                my $next_char = shift @chars;
+                if ( $next_char eq $LEFT_PARENTHESIS ) {
+                    $token .= $next_char;
+                    push @tokens, $token;
+                }
+                elsif ( $next_char eq $QUESTION_MARK ) {
+                    $token .= $next_char;
+                    my $next_char = shift @chars;
+                    if ( $next_char eq $LEFT_PARENTHESIS ) {
+                        $token .= $next_char;
+                        push @tokens, $token;
+                    }
+                    else {
+                        die qq{filter operator "$token" must be followed by '('\n};
+                    }
+                }
+                else {
+                    unshift @chars, $next_char;
+                    push @tokens, $token;
+                }
+            }
+            elsif ( $char eq $RIGHT_PARENTHESIS ) {
+                my $next_char = shift @chars;
+                my $token     = $char;
+                if ( $next_char eq $RIGHT_SQUARE_BRACKET ) {
+                    $token .= $next_char;
+                    push @tokens, $token;
+                }
+                else {
+                    die qq{Unterminated expression: '[(' or '[?(' without corresponding ')]'\n};
+                }
+            }
+            else {
+                push @tokens, $char;
+            }
         }
     }
 
     return @tokens;
 }
 
+# my %EXPRESSIONS = (
+#     # map { $_->{id} } ITEMAT( $root, 'ALL' )
+#     q{$.[*].id}                                   => ['$', '*', 'id'],
+#
+#     # map { $_->{id} } ITEMAT( $root, 0 )
+#     q{$.[0].title}                                => ['$', '0', 'title'],
+#
+#     # grep { $_.login eq 'laurilehmijoki'}  map { $_->{user} } ITEMAT( $root, 'ALL' )
+#     q{$.[*].user[?(@.login == 'laurilehmijoki')]} => ['$', '*', 'user',    'FILTER(', '@',  q{login == 'laurilehmijoki'}, ')'],
+#
+#     # map { $_->{title} } grep { $_.price < 10 } map { $_->{book} } map { $_->{store} } $root
+#     q{$.store.book[?(@.price < 10)].title}        => ['$', 'store', 'book',  'FILTER(', '@', q{price < 10}, ')', 'title'],
+#
+#     # grep { $_->{name} eq 'bug' } RECURSIVE( { exists $_->{labels} }, $root )
+#     q{$..labels[?(@.name==bug)]}                  => ['$', '..', 'labels',   'FILTER(', '@',  q{name==bug}, ')'],
+#
+#     # grep { $_->{addresstype}{id} eq "D84002" } map { $_->{addresses} } $root
+#     q{$.addresses[?(@.addresstype.id == D84002)]} => ['$', 'addresses',      'FILTER(', '@', 'addresstype', q{id == D84002}, ')'],
+#
+#     # map { $_->{title} } ITEMAT( $_, length (@$_) - 1 )
+#     q{$.store.book[(@.length-1)].title}           => ['$', 'store', 'book',  'SCRIPTENGINE(', '@', q{length-1}, ')', 'title'],
+#
+#     # map { $->{author} } ITEMAT( $_, 0 ) map { $_->{book} } map { $_->{store} } $root
+#     q{$['store']['book'][0]['author']}            => ['$', 'store', 'book', '0', 'author'],
+#
+#     # map { $->{author} } ITEMAT( $_, 1 ) map { $_->{book} } map { $_->{store} } $root
+#     q{$['store']['book'][1]['author']}            => ['$', 'store', 'book', '1', 'author'],
+# );
 sub normalize {
-    my $expression = shift;
+    my @tokens = @_;
 
+    my @normalized;
+    my @stack;
 
-    ( my $normalized = $expression ) =~ s/\s+//g;
-    my $regex = qr/
-        \[          # match opening bracket
-        ['|"]{0,1}  # quote
-        (\w+|\d+)   # index or hash key
-        ['|"]{0,1}  # quote
-        \]          # match closing bracket
-    /x;
-    
-    $normalized =~ s/$regex/.$1/gx;
-
-    return $normalized;
 }
 
 1;
--- t/compiler/tokenize.t
+++ t/compiler/tokenize.t
@@ -4,22 +4,22 @@ use Test::Most;
 use JSON::Path::Compiler;
 
 my %EXPRESSIONS = (
-    q{$.[*].id}                                   => [qw/$ . [ * ] . id/],
-    q{$.[0].title}                                => [qw/$ . [ 0 ] . title/],
-    q{$.[*].user[?(@.login == 'laurilehmijoki')]} => [qw/$ . [ * ] . user [ ? ( @ . /, q{login == 'laurilehmijoki'}, qw/) ]/],
-    q{$..labels[?(@.name==bug)]}                  => [qw/$ .. labels [ ? ( @ . name==bug ) ]/],
-    q{$.addresses[?(@.addresstype.id == D84002)]} => [qw/$ . addresses [ ? ( @ . addresstype . /, 'id == D84002', qw/) ]/],
-    q{$.store.book[(@.length-1)].title}           => [qw/$ . store . book [ ( @ . length-1 ) ] . title/],
-    q{$.store.book[?(@.price < 10)].title}        => [qw/$ . store . book [ ? ( @ ./,  'price < 10', qw/) ] . title/],
-    q{$['store']['book'][0]['author']}            => [qw/$ [ 'store' ] [ 'book' ] [ 0 ] [ 'author' ]/],
-    q{$['store']['book'][1]['author']}            => [qw/$ [ 'store' ] [ 'book' ] [ 1 ] [ 'author' ]/],
-    q{$['store']['book'][2]['author']}            => [qw/$ [ 'store' ] [ 'book' ] [ 2 ] [ 'author' ]/],
-    q{$['store']['book'][3]['author']}            => [qw/$ [ 'store' ] [ 'book' ] [ 3 ] [ 'author' ]/],
+    q{$.[*].id}                                   => ['$', '.',     '[',            '*',    ']',    '.',    'id'],
+    q{$.[0].title}                                => ['$', '.',     '[',            '0',    ']',    '.',    'title'],
+    q{$..labels[?(@.name==bug)]}                  => ['$', '..',    'labels',       '[?(',  '@',    '.',    'name==bug',    ')]'],
+    q{$.addresses[?(@.addresstype.id == D84002)]} => ['$', '.',     'addresses',    '[?(',  '@',    '.',    'addresstype',  '.',         'id == D84002', ')]'],
+    q{$.store.book[(@.length-1)].title}           => ['$', '.',     'store',        '.',    'book', '[(',   '@',            '.',         'length-1',     ')]', '.',     'title'],
+    q{$.store.book[?(@.price < 10)].title}        => ['$', '.',     'store',        '.',    'book', '[?(',  '@',            '.',         'price < 10',   ')]', '.',     'title'],
+    q{$['store']['book'][0]['author']}            => ['$', '[',     'store',        ']',    '[',    'book', ']',            '[',         0,              ']', '[',      'author', ']'],
+    q{$['store']['book'][1]['author']}            => ['$', '[',     'store',        ']',    '[',    'book', ']',            '[',         1,              ']', '[',      'author', ']'],
+    q{$['store']['book'][2]['author']}            => ['$', '[',     'store',        ']',    '[',    'book', ']',            '[',         2,              ']', '[',      'author', ']'],
+    q{$['store']['book'][3]['author']}            => ['$', '[',     'store',        ']',    '[',    'book', ']',            '[',         3,              ']', '[',      'author', ']'],
+    q{$.[*].user[?(@.login == 'laurilehmijoki')]} => ['$', '.',     '[',            '*',    ']',    '.',    'user',         '[?(',       '@',             '.', q{login == 'laurilehmijoki'}, ')]'],
 );
 
 for my $expression (keys %EXPRESSIONS) {
-    my @tokens = JSON::Path::Compiler::tokenize($expression);
-    is_deeply \@tokens, $EXPRESSIONS{$expression}, qq{Expression "$expression" tokenized correctly};
+    my @tokens;
+    lives_and { is_deeply [ JSON::Path::Compiler::tokenize($expression) ], $EXPRESSIONS{$expression} } qq{Expression "$expression" tokenized correctly};
 }
 
 done_testing;
